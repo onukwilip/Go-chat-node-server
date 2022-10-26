fe69e57e0605f4215eb95b7056a8c7dbc79911fb
Prince <onukwilip@gmail.com>
2022-10-26 17:52:52 +0100

    This is my first upload

---
+++ server.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3002;
+var http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://admin.socket.io",
+    ],
+    methods: ["GET", "POST"],
+    "Access-Control-Allow-Credentials": true,
+  },
+});
+
+const userIo = io.of("/user");
+
+const chatRoomIo = io.of("/chatroom");
+
+const discussionIo = io.of("/discussion");
+
+const socketHandler = (socket) => {
+  socket.emit("connection", (message = "You are connected successfully"));
+  console.log(`USER ${socket.id} CONNECTED SUCCESSFULLY`);
+
+  socket.on("disconnection", () => {
+    console.log(`USER ${socket.id} DISCONNECTED SUCCESSFULLY`);
+  });
+};
+
+const userHandler = (socket) => {
+  socket.emit(
+    "connection",
+    (message = "You are connected to user namespace successfully")
+  );
+  console.log(`USER ${socket.id} CONNECTED TO USER NAMESPACE SUCCESSFULLY`);
+
+  socket.on("disconnection", () => {
+    console.log(
+      `USER ${socket.id} DISCONNECTED FROM USER NAMESPACE SUCCESSFULLY`
+    );
+  });
+};
+
+const chatroomHandler = (socket) => {
+  socket.emit(
+    "connection",
+    (message = "You are connected to chatroom namespace successfully")
+  );
+  console.log(`USER ${socket.id} CONNECTED TO CHATROOM NAMESPACE SUCCESSFULLY`);
+
+  socket.on("disconnection", () => {
+    console.log(
+      `USER ${socket.id} DISCONNECTED FROM CHATROOM NAMESPACE SUCCESSFULLY`
+    );
+  });
+
+  socket.on("chat", (chat) => {
+    chatRoomIo
+      .to(chat?.ChatroomID)
+      .emit("chat_sent", (message = "chat recieved"));
+  });
+
+  socket.on("join", (roomID) => {
+    socket.join(roomID);
+    console.log(`USER WITH ${socket?.id} JOINED CHATROOM ${roomID}`);
+  });
+};
+
+const discussionHandler = (socket) => {
+  socket.emit(
+    "connection",
+    (message = "You are connected to discussion namespace successfully")
+  );
+  console.log(
+    `USER ${socket.id} CONNECTED TO DISCUSSION NAMESPACE SUCCESSFULLY`
+  );
+
+  socket.on("disconnection", () => {
+    console.log(
+      `USER ${socket.id} DISCONNECTED FROM DISCUSSION NAMESPACE SUCCESSFULLY`
+    );
+  });
+
+  socket.on("new-discussion", () => {
+    discussionIo.emit("new-discussion");
+  });
+};
+
+discussionIo.on("connection", discussionHandler);
+
+userIo.on("connection", userHandler);
+
+chatRoomIo.on("connection", chatroomHandler);
+
+io.on("connection", socketHandler);
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
